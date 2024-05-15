b7567a58ce81edcc77fb696089588f265c4b0e25
Daikku <vadess2013@mail.ru>
2024-05-15 23:36:25 +0300

    Added connect to database mongo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Telegraf } from "telegraf";
 import { config } from "./config";
+import { connectToDatabase } from "./database";
 
 const bot = new Telegraf(config.botToken as string)
 
@@ -7,8 +8,10 @@ bot.start((ctx): void => {
     ctx.reply('Hello!')
 })
 
-bot.launch()
-console.log('Bot is running')
+connectToDatabase().then(() =>{
+    bot.launch()
+    console.log('Bot is running')
+})
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
