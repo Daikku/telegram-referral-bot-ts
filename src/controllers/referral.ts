ee3b965a446c9ea0a8b4cb3492a87ad3dc0abe2f
Daikku <vadess2013@mail.ru>
2024-05-23 00:08:50 +0300

    Sending messages to the user and his referral has been made into a separate function

--- src/controllers/referral.ts
+++ src/controllers/referral.ts
@@ -1,10 +1,25 @@
 import { Context } from 'telegraf';
 import { User } from '../models/user';
 
-export const referralCommand = async (ctx: Context): Promise<void> => {
+export const referralLinkCommand = async (ctx: Context): Promise<void> => {
     const telegramId = ctx.from?.id as number;
-    const user = await User.findOne({ telegramId });
+    const user = await User.findOne({ telegramId }).lean();
     if (user) {
         await ctx.reply(`Ваша реферальная ссылка: ${user.referralLink}`);
     }
+};
+
+export const sendReferralMessage = async (telegramId: number, referredBy: number, ctx: Context): Promise<void> => {
+    await User.updateOne({ telegramId: referredBy },
+        {
+            $addToSet: { directReferrals: telegramId },
+            $inc: {balance: 10}
+        });
+
+    const referrer = await User.findOne({telegramId: referredBy}).lean();
+    await ctx.reply(`Добро пожаловать! Вас пригласил ${ referrer?.telegramId }`)
+    await ctx.telegram.sendMessage(
+        referredBy,
+        `Новый пользователь ${ctx.from?.first_name} присоединился`
+    )
 };
